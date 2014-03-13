a4b2ba054181149803ed7c85ba40cda76a6880f9
Brent Meyer <brent.e.meyer@gmail.com>
2014-03-12 22:11:41 -0400

    finishes user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
 
   validates :password, length: { minimum: 6 }
+  validates :password_confirmation, length: { minimum: 6 }
 
   has_secure_password
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,4 +12,43 @@ describe "UserPages" do
     end
   end
 
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it "should be the user's profile" do
+      expect(page).to have_content(user.name)
+    end
+  end
+
+  describe "signup" do
+    before { visit signup_path }
+    let(:submit) { "Sign up" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",             with: "New User"
+        fill_in "Email",            with: "brent.e.meyer@gmail.com"
+        fill_in "Password",         with: "password"
+        fill_in "Confirm password", with: "password"
+      end
+
+      it "should create a new user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
