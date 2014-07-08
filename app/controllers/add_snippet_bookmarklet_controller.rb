6e28301939fc3e7897f26f5a3aa28484e4dbafdf
Brent Meyer <brent.e.meyer@gmail.com>
2014-07-08 00:12:30 -0400

    wip, tests and first attempt front end

--- app/controllers/add_snippet_bookmarklet_controller.rb
+++ app/controllers/add_snippet_bookmarklet_controller.rb
@@ -4,19 +4,20 @@ class AddSnippetBookmarkletController < ApplicationController
   end
 
   def create
-    if current_user
+    if signed_in?
       snippet = current_user.snippets.build(snippet_params)
+      snippet.content = ActionView::Base::full_sanitizer.sanitize(snippet.content)
       if snippet.save
         snippet.create_activity :create, owner: current_user, parameters: { snippet_content: snippet.content }
 
-        response = { status: "success" }
+        response = { status: "success", url: snippet_url(snippet) }
         render json: response.to_json, callback: params['callback']
       else
-        response = { status: "fail", error: "Snippet couldn't be saved. Try again later." }
+        response = { status: "fail", error: "That snippet couldn't be saved." }
         render json: response.to_json, callback: params['callback']
       end
     else
-      response = { status: "fail", error: "Please sign in first" }
+      response = { status: "no-user", error: "Please sign in first", url: signin_url }
       render json: response.to_json, callback: params['callback']
     end
   end
--- spec/controllers/add_snippet_bookmarklet_controller_spec.rb
+++ spec/controllers/add_snippet_bookmarklet_controller_spec.rb
@@ -2,4 +2,38 @@ require 'spec_helper'
 
 describe AddSnippetBookmarkletController do
 
+  let(:user) { FactoryGirl.create(:user) }
+
+  before { sign_in user, no_capybara: true }
+
+  describe "GET create" do
+
+    describe "with appropriate params" do
+
+      it "should add a snippet" do
+        expect do
+          xhr :get, :create, snippet: { source: "http://google.com", content: "a" * 51 }
+        end.to change(Snippet, :count).by(1)
+      end
+
+      it "should respond with success" do
+        xhr :get, :create, snippet: { source: "http://google.com", content: "a" * 51 }
+        expect(response).to be_success
+      end
+    end
+
+    describe "with bad params" do
+
+      it "should not add a snippet" do
+        expect do
+          xhr :get, :create, snippet: { source: "", content: "" }
+        end.not_to change(Snippet, :count).by(1)
+      end
+
+      it "should not respond with success" do
+        xhr :get, :create, snippet: { source: "", content: "" }
+        expect(response.body).to have_content "fail"
+      end
+    end
+  end
 end
