550debb614957990de8888b90e63c6c93300da48
Brent Meyer <brent.e.meyer@gmail.com>
2014-07-08 00:33:56 -0400

    wip, more bookmarklet tests

--- spec/controllers/add_snippet_bookmarklet_controller_spec.rb
+++ spec/controllers/add_snippet_bookmarklet_controller_spec.rb
@@ -4,11 +4,25 @@ describe AddSnippetBookmarkletController do
 
   let(:user) { FactoryGirl.create(:user) }
 
-  before { sign_in user, no_capybara: true }
-
   describe "GET create" do
 
+    describe "when not signed in" do
+
+      it "should not add a snippet" do
+        expect do
+        xhr :get, :create, snippet: { source: "http://google.com", content: "a" * 51 }
+        end.not_to change(Snippet, :count).by(1)
+      end
+
+      it "should not respond with success" do
+        xhr :get, :create, snippet: { source: "http://google.com", content: "a" * 51 }
+        expect(response.response_code).to eq 401
+      end
+    end
+
     describe "with appropriate params" do
+
+      before { sign_in user, no_capybara: true }
 
       it "should add a snippet" do
         expect do
@@ -18,11 +32,13 @@ describe AddSnippetBookmarkletController do
 
       it "should respond with success" do
         xhr :get, :create, snippet: { source: "http://google.com", content: "a" * 51 }
-        expect(response).to be_success
+        expect(response.response_code).to eq 200
       end
     end
 
     describe "with bad params" do
+
+      before { sign_in user, no_capybara: true }
 
       it "should not add a snippet" do
         expect do
@@ -32,7 +48,7 @@ describe AddSnippetBookmarkletController do
 
       it "should not respond with success" do
         xhr :get, :create, snippet: { source: "", content: "" }
-        expect(response.body).to have_content "fail"
+        expect(response.response_code).to eq 400
       end
     end
   end
